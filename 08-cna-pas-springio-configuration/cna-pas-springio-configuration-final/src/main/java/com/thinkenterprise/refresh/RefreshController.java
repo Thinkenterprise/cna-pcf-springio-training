b2406cde20110765ccfddc98d260ab0712c0e73b
Thinkenterprise <michael.schaefer@msg-systems.com>
2020-03-07 12:31:11 +0100

    Add Session Configuration

---
+++ 08-cna-pas-springio-configuration/cna-pas-springio-configuration-final/src/main/java/com/thinkenterprise/refresh/RefreshController.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2019 Thinkenterprise
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * @author Michael Schaefer
+ */
+package com.thinkenterprise.refresh;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thinkenterprise.Application;
+
+@RefreshScope
+@RestController
+public class RefreshController {
+
+	private Log logger = LogFactory.getLog(RefreshController.class);
+
+	@Value("${aero.gateway.enabled}")
+	private Boolean enabled;
+
+	@RequestMapping("enabled")
+	public Boolean enabled() {
+		return this.enabled;
+
+	}
+
+	@EventListener
+	public void refresh(RefreshScopeRefreshedEvent e) {
+		this.logger.info("The Property will be refreshed " + e.getTimestamp() + ":" +  enabled);
+	}
+
+}
